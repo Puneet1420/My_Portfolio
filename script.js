7be46941dbfe33d5ae32691138546068c6599625
Puneet1420 <puneetbajpai1420@gmail.com>
2023-08-10 00:55:08 +0530

    gsap animation added

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ function circlemousefollower (){
     })
 }
 circlemousefollower();
+
+// gsap style for animation
+let tl = gsap.timeline();
+
+tl.from("#nav a, #nav h2",{
+    y:30,
+    duration:0.3,
+    delay:0.3,
+    opacity:0,
+})
+tl.from("#heading h1, #blocktext h1",{
+    y:30,
+    duration:0.3,
+    delay:0.3,
+    opacity:0,
+    stagger:0.3
+})
+tl.from("#blocktext h3,#smallheading h3,#herofooter ",{
+    y:-30,
+    duration:0.3,
+    delay:0.3,
+    opacity:0,
+    stagger:0.3
+
+})
