f33f05806399e46e64651393315817d1626e999f
Puneet1420 <puneetbajpai1420@gmail.com>
2023-08-11 01:59:08 +0530

    created mouse pointer and fixed it

--- script.js
+++ script.js
@@ -3,12 +3,39 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-function circlemousefollower (){
+//flat mouse pointer code and logic...........
+let timeout;
+function flatcircle (){
+    let xscale = 1;
+    let yscale = 1;
+
+    let xprev = 0;
+    let yprev = 0;
+
+window.addEventListener("mousemove", function (details){
+    clearTimeout(timeout);
+    xscale = gsap.utils.clamp(.8, 1.2, details.clientX - xprev);
+    yscale = gsap.utils.clamp(.8, 1.2, details.clientY - yprev);
+    
+    xprev = details.clientX;
+    yprev = details.clientY;
+
+    circlemousefollower (xscale, yscale);
+    this.setTimeout(function(){
+      timeout =  document.querySelector("#minicircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px) scale(1,1)`;
+    },100)
+
+});
+}
+flatcircle ();
+// mouse follower
+function circlemousefollower (xscale, yscale){
     window.addEventListener("mousemove",function(details){
-    document.querySelector("#minicircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px)`;
+    document.querySelector("#minicircle").style.transform = `translate(${details.clientX}px, ${details.clientY}px) scale(${xscale},${yscale})`;
     })
 }
 circlemousefollower();
+
 
 // gsap style for animation
 let tl = gsap.timeline();
